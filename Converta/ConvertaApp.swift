1ef4f4a4c314a28c206ad1c490fd5297db03ccde
ChaoMixian <chaomingxun@outlook.com>
2025-07-03 00:56:22 +0800

    Initial Commit

---
+++ Converta/ConvertaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertaApp.swift
+//  Converta
+//
+//  Created by 陈铭勋 on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ConvertaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConvertaTests/ConvertaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertaTests.swift
+//  ConvertaTests
+//
+//  Created by 陈铭勋 on 7/3/25.
+//
+
+import Testing
+@testable import Converta
+
+struct ConvertaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
